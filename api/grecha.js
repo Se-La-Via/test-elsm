0c6e86f08efccdfd28bb0714b1fd8e2cc2af3ad5
Ayder Aliev <ayder.aliev@mgid.com>
2025-06-22 09:33:52 +0300

    parameter transfer from frontend

--- api/grecha.js
+++ api/grecha.js
@@ -9,8 +9,8 @@ const DEFAULT_SKIP  = 0;
 
 export default async function handler(req, res) {
     // Read parameters from query (if not set, use defaults)
-    const wallet = Number(req.query.wallet_id) || DEFAULT_WALLET;
-    const coin  = Number(req.query.symbol)  || DEFAULT_SYMBOL;
+    const wallet = Number(req.query.wallet) || DEFAULT_WALLET;
+    const coin  = Number(req.query.coin)  || DEFAULT_SYMBOL;
     const limit = Number(req.query.limit) || DEFAULT_LIMIT;
     const skip  = Number(req.query.skip)  || DEFAULT_SKIP;
 
